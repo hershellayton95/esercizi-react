dac32482c539ceb621381d4660f84b639f87f8ca
Filippo Di Marco <filidm5795@gmail.com>
2022-11-09 13:33:34 +0100

    create props 03

--- src/Welcome.js
+++ src/Welcome.js
@@ -5,14 +5,8 @@ export class Welcome extends React.Component {
     render() {
         return (
             <div>
-                {/* <p>"Welcome, {this.props.name ?? "Dude"}"</p> */}
-
-                {/* {!!this.props.name && <p>Welcome, {this.props.name}</p>}
-                {!this.props.name && <p>Welcome, Dude</p>} */}
-
-                {!!this.props.name
-                ? <p>"Welcome, {this.props.name}"</p>
-                :<p>"Welcome, Dude"</p>}
+                <p>Welcome, {this.props.name ?? "Dude"}</p>
+                {this.props.age && <p>Your age is {this.props.age}</p>}
             </div>
         )
     }
