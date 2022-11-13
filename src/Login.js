0eb14a6e7a55f4a85515d61e285bbcc46d684013
Filippo Di Marco <filidm5795@gmail.com>
2022-11-13 19:54:56 +0100

    create form 3

--- src/Login.js
+++ src/Login.js
@@ -21,12 +21,23 @@ export class Login extends React.Component {
         })
     }
 
+    onLogin = () => {
+        this.setState({
+                username: this.state.username,
+                password: this.state.password,
+                remember: this.state.remember
+        })
+    }
+
     render() {
         return (
             <div>
-                <input type="text" name="username" onChange={this.handlerInput} />
-                <input type="password" name="password" onChange={this.handlerInput} />
-                <input type="checkbox" name="remember" onChange={this.handlerInput} />
+                <input type="text" name="username" value={this.state.username} onChange={this.handlerInput} />
+                <input type="password" name="password" value={this.state.password}  onChange={this.handlerInput} />
+                <input type="checkbox" name="remember" checked={this.state.remember}  onChange={this.handlerInput} />
+                <div>
+                    <button type="button" onClick={this.onLogin} disabled={!this.state.username || !this.state.password}>Login</button>
+                </div>
             </div>
         )
     }
