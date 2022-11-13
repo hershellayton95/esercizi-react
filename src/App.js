e622c54cd79917a2922119140471da70aa363b7a
Filippo Di Marco <filidm5795@gmail.com>
2022-11-13 20:45:40 +0100

    create form 5

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Login } from "./Login";
+import { UncontrolledLogin } from "./UncontrolledLogin";
 
 export class App extends React.Component {
     render() {
 
         return (
             <div>
-                <Login />
+                <UncontrolledLogin />
             </div>
         )
     }
---
+++ src/UncontrolledLogin.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export class UncontrolledLogin extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: false,
+            password: false
+        }
+    }
+
+    headlerSubmin = (event) => {
+        event.preventDefault();
+
+        const username = event.target.elements.username.value;
+        const password = event.target.elements.password.value;
+        const remember = event.target.elements.remember.checked;
+
+        console.log({
+            username,
+            password,
+            remember
+        });
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.headlerSubmin}>
+                <input type="text" name="username"/>
+                <input type="password" name="password"/>
+                <input type="checkbox" name="remember"/>
+                <div>
+                    <button type="submit">Login</button>
+                    <button type="reset">Reset</button>
+                </div>
+            </form>
+        )
+    }
+}
