c6ec364272e5e5874e3d2f83e43a48887d6744ac
Filippo Di Marco <filidm5795@gmail.com>
2022-11-19 17:34:04 +0100

    create style components 03

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Welcome } from "./Welcome";
+import { Login } from "./Login";
 
 export class App extends React.Component {
     render() {
 
         return (
             <div>
-                <Welcome class={"welcome"} />
+                <Login />
             </div>
         )
     }
