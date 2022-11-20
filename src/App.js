c7d9449e056f2cf2156a3038c75b4ce8b142dfdb
Filippo Di Marco <filidm5795@gmail.com>
2022-11-20 12:17:54 +0100

    create component composition 01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
-import { Login } from "./Login";
+import { Container } from "./Container";
 
 export class App extends React.Component {
     render() {
 
         return (
             <div>
-                <Login />
+                <Container>
+                    <div></div>
+                </Container>
             </div>
         )
     }
