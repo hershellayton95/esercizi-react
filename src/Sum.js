056b8969f451370d9788fcc264d3a2cc79258ccb
Filippo Di Marco <filidm5795@gmail.com>
2022-11-23 23:46:49 +0100

    update function component 02

--- src/Sum.js
+++ src/Sum.js
@@ -1,5 +1,5 @@
-export function Sum({number}){
-    const sum = number.reduce((prev, next)=>prev + next,0);
+export function Sum(props){
+    const sum = props.number.reduce((prev, next)=>prev + next,0);
     return (
         <h1>{sum}</h1>
     )
