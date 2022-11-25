47fee5c0647cfff8b636a0afbed70c3391065a52
Filippo Di Marco <filidm5795@gmail.com>
2022-11-25 11:43:46 +0100

    create use effect 04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,25 +1,39 @@
 import { useEffect, useState } from "react";
 
 
-export function GithubUser({username}) {
-    const[date, setData] = useState(null);
+export function GithubUser({ username }) {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(false);
 
-    async function fetchFunction(username){
-        const response = await fetch(`https://api.github.com/users/${username}`);
-        const result = await response.json();
-        setData(result);
+    async function fetchFunction(username) {
+        setError(false);
+        setData(null);
+        try {
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            // const response = await fetch(`http://localhost:3004/${username}`);
+
+            if (response.status !== 200) {
+                throw new Error();
+            }
+            setData(await response.json());
+        } catch (error) {
+            setError(true);
+        }
+
     }
-    useEffect(()=> {
-        fetchFunction(username)
-        return ()=>{
+
+    useEffect(() => {
+        fetchFunction(username);
+        return () => {
             setData(null)
         }
-    },[username])
+    }, [username])
 
     return (
         <div>
-            {date && <p>username:{date.login}</p>}
-            {date && <p>name: {date.name}</p>}
+            {!error && !!data && <p>username:{data.login}</p>}
+            {!error && !!data && <p>name: {data.name}</p>}
+            {error && <p>Something has gon wrong</p>}
         </div>
     )
 }
