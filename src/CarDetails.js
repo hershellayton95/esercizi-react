a25440cb5790e16df5ecea49436c9879ea27ba4d
Filippo Di Marco <filidm5795@gmail.com>
2022-11-27 09:56:16 +0100

    update use_ref_02

--- src/CarDetails.js
+++ src/CarDetails.js
@@ -11,8 +11,15 @@ export function CarDetails({ initialData = { model: "Panda 4x4", year: 1994, col
         resetFunction();
     },[initialData])
 
+    const hadleFormValues = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+
+        _formRef.current[name].value = value;
+    }
+
     return (
-        <form ref={_formRef}>
+        <form ref={_formRef} onChange={hadleFormValues}>
             <input type="text" name="model" defaultValue={initialData.model} />
             <input type="number" name="year" defaultValue={initialData.year} />
             <input type="text" name="color" defaultValue={initialData.color} />
