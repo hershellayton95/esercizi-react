df575acaa359941acf510e20f45a82a467d62cf8
Filippo Di Marco <filidm5795@gmail.com>
2022-11-28 10:47:25 +0100

    react_router_05

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ export function App() {
                 <Route path='/' element={<Welcome name="Filippo" />} />
                 <Route path='/counter' element={<Counter />} />
                 <Route path="users/:username" element={<ShowGithubUser />}/>
+                <Route path="*" element={<div>Page not found. Error 404</div>}/>
             </Routes>
     
                 <Link to="/">Home page</Link> | <Link to="/counter">Counter</Link> | <Link to="/users/hershellayton">hershellayton95 Github Info</Link>
