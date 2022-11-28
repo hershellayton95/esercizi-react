7bae06cb5c4f5fd7fd2bf80ad95b9612e67453bc
Filippo Di Marco <filidm5795@gmail.com>
2022-11-28 10:42:49 +0100

    react_router_04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import { Counter } from "./Counter";
 import { ShowGithubUser } from "./ShowGithubUser";
 import { Welcome } from "./Welcome";
@@ -6,10 +6,14 @@ import { Welcome } from "./Welcome";
 export function App() {
 
     return (
-        <Routes>
-            <Route path='/' element={<Welcome name="Filippo" />} />
-            <Route path='/counter' element={<Counter />} />
-            <Route path="users/:username" element={<ShowGithubUser />}/>
-        </Routes>
+        <div>
+            <Routes>
+                <Route path='/' element={<Welcome name="Filippo" />} />
+                <Route path='/counter' element={<Counter />} />
+                <Route path="users/:username" element={<ShowGithubUser />}/>
+            </Routes>
+    
+                <Link to="/">Home page</Link> | <Link to="/counter">Counter</Link> | <Link to="/users/hershellayton">hershellayton95 Github Info</Link>
+        </div>
     )
 }
